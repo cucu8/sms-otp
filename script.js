0ccf258e83dab4939ef7c3c4b8a3950c57749b2c
cuneyt.donmez <cuneyt.donmez@btctrader.com>
2025-04-29 14:01:40 +0300

    asd

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const otpInput = document.getElementById("otpInput");
-const status = document.getElementById("status");
-
-if ("OTPCredential" in window) {
-  window.addEventListener("DOMContentLoaded", async () => {
+async function startOTP() {
+  if ("OTPCredential" in window) {
     try {
       const ac = new AbortController();
       const otp = await navigator.credentials.get({
         otp: { transport: ["sms"] },
         signal: ac.signal,
       });
-      otpInput.value = otp.code;
-      status.textContent = "Kod otomatik alındı!";
+      alert(JSON.stringify(otp));
+
+      document.getElementById("otpInput").value = otp.code;
+      document.getElementById("status").textContent = "Kod alındı!";
     } catch (err) {
-      status.textContent = "Kod alınamadı: " + err.message;
+      document.getElementById("status").textContent =
+        "Kod alınamadı: " + err.message;
     }
-  });
-} else {
-  status.textContent = "Web OTP API desteklenmiyor.";
+  } else {
+    document.getElementById("status").textContent =
+      "Web OTP API desteklenmiyor.";
+  }
 }
