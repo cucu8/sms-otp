ad66d7d64ccdabcc2ada89aea711032c7e4be4ef
cuneyt.donmez <cuneyt.donmez@btctrader.com>
2025-04-29 14:39:16 +0300

    1

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 const otpInput = document.getElementById("otpInput");
 
 if ("OTPCredential" in window) {
-  window.addEventListener("DOMContentLoaded", async () => {
-    alert("document loaded");
-    try {
-      const ac = new AbortController();
-      const otp = await navigator.credentials.get({
+  window.addEventListener("DOMContentLoaded", (e) => {
+    const input = document.querySelector('input[autocomplete="one-time-code"]');
+    const textValue = document.querySelector(".js-text");
+    if (!input) return;
+    const ac = new AbortController();
+    const form = input.closest("form");
+    if (form) {
+      form.addEventListener("submit", (e) => {
+        ac.abort();
+      });
+    }
+    navigator.credentials
+      .get({
         otp: { transport: ["sms"] },
         signal: ac.signal,
+      })
+      .then((otp) => {
+        alert(JSON.stringify(otp));
+        debugger;
+        textValue.value = JSON.stringify(otp);
+        input.value = otp.code;
+        if (form) form.submit();
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(JSON.stringify(err));
+
+        textValue.value = JSON.stringify(err);
       });
-
-      alert(JSON.stringify(otp));
-
-      otpInput.value = otp.code;
-    } catch (err) {
-      alert(JSON.stringify(error));
-    }
   });
-} else {
-  alert("olmadı baştan");
 }
